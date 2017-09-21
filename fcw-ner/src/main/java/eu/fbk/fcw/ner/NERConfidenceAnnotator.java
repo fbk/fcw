79eb9bca74655d64994c702b067e9cd8b619ddfd
rospocher <rospocher@fbk.eu>
2017-09-21 10:35:59 +0200

    urevised download instructions

--- fcw-ner/src/main/java/eu/fbk/fcw/ner/NERConfidenceAnnotator.java
+++ fcw-ner/src/main/java/eu/fbk/fcw/ner/NERConfidenceAnnotator.java
@@ -3,10 +3,13 @@ package eu.fbk.fcw.ner;
 import java.io.IOException;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 
+import edu.stanford.nlp.ie.NERClassifierCombiner;
+import edu.stanford.nlp.ie.regexp.NumberSequenceClassifier;
 import edu.stanford.nlp.util.*;
 import org.apache.log4j.Logger;
 
@@ -32,11 +35,14 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
 
     private static final double DEFAULT_MIN_SPAN_CONFIDENCE = 0.5;
 
+    private static final boolean DEFAULT_NORMALIZE_PROBABILITIES = false;
+
     private static final Set<String> DEFAULT_PASS_DOWN_PROPERTIES = CollectionUtils.asSet(
             "encoding", "inputEncoding", "outputEncoding", "maxAdditionalKnownLCWords", "map",
-            "ner.combinationMode");
+            "ner4KBest.combinationMode");
 
-    private final AbstractSequenceClassifier<CoreLabel> ner;
+    private final AbstractSequenceClassifier<CoreLabel> ner4KBest;
+    private final NERClassifierCombiner nerStandard;
 
     private final long maxTime;
 
@@ -48,20 +54,27 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
 
     private final double minSpanConfidence;
 
+    private final boolean normalizeProbabilities;
+
     public NERConfidenceAnnotator(final String name, final Properties properties) {
         try {
-            this.ner = CRFClassifier.<CoreLabel>getClassifier(DefaultPaths.DEFAULT_NER_CONLL_MODEL,
+//            change the model here?
+            this.nerStandard = NERClassifierCombiner.createNERClassifierCombiner(name, properties);
+            this.ner4KBest = CRFClassifier.<CoreLabel>getClassifier(DefaultPaths.DEFAULT_NER_CONLL_MODEL,
                     PropertiesUtils.extractSelectedProperties(properties,
                             DEFAULT_PASS_DOWN_PROPERTIES));
             this.maxTime = PropertiesUtils.getLong(properties, name + ".maxtime", -1);
             this.nThreads = PropertiesUtils.getInt(properties, name + ".nthreads",
-                    PropertiesUtils.getInt(properties, "nthreads", 1));
+                    PropertiesUtils.getInt(properties, ".nthreads", 1));
             this.maxSentenceLength = PropertiesUtils.getInt(properties, name + ".maxlength",
                     Integer.MAX_VALUE);
             this.maxLabelings = PropertiesUtils.getInt(properties, name + ".maxLabelings",
                     DEFAULT_MAX_LABELINGS);
             this.minSpanConfidence = PropertiesUtils.getDouble(properties,
-                    name + "minSpanConfidence", DEFAULT_MIN_SPAN_CONFIDENCE);
+                    name + ".minSpanConfidence", DEFAULT_MIN_SPAN_CONFIDENCE);
+            this.normalizeProbabilities = PropertiesUtils.getBool(properties,
+                    name + ".normalizeProbabilities", DEFAULT_NORMALIZE_PROBABILITIES);
+
         } catch (final ClassNotFoundException | IOException ex) {
             throw new IllegalArgumentException(ex);
         }
@@ -80,7 +93,7 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
     @Override
     public void annotate(final Annotation annotation) {
         super.annotate(annotation);
-        // nsc.finalizeClassification(annotation); TODO
+        this.ner4KBest.finalizeClassification(annotation);
     }
 
     @Override
@@ -95,17 +108,62 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
             return;
         }
 
+        //for the best output;
+        List<CoreLabel> output;
         try {
+            //get the best annotation from stanford
+            output = this.nerStandard.classifySentenceWithGlobalInformation(tokens, annotation, sentence);
+
+            for (int i = 0, sz = tokens.size(); i < sz; ++i) {
+                // add the named entity tag to each token
+                String neTag = output.get(i).get(CoreAnnotations.NamedEntityTagAnnotation.class);
+                String normNeTag = output.get(i).get(CoreAnnotations.NormalizedNamedEntityTagAnnotation.class);
+                tokens.get(i).setNER(neTag);
+                if (normNeTag != null) tokens.get(i).set(CoreAnnotations.NormalizedNamedEntityTagAnnotation.class, normNeTag);
+                NumberSequenceClassifier.transferAnnotations(output.get(i), tokens.get(i));
+            }
+
+            //retrieve the spanning produced by the standard version of NER CoreNLP
+            final Map<Span, Counter<String>> entitiesStandard =  Maps.newHashMap();
+            extractEntitiesFromSingleLabelling(entitiesStandard,output,1.0); //fake 1.0 prob
+
+            //printing the spans
+            if (LOGGER.isDebugEnabled()) {
+//            if (true) {
+                final StringBuilder builder = new StringBuilder();
+                builder.append("Standard NER annotation for \"").append(annotation).append("\":");
+                for (final Entry<Span, Counter<String>> entry : entitiesStandard.entrySet()) {
+                    final Span span = entry.getKey();
+                    final Counter<String> counter = entry.getValue();
+                    builder.append("\n  ");
+                    for (int i = span.begin; i < span.end; ++i) {
+                        builder.append(tokens.get(i));
+                    }
+                    builder.append(" = ").append(counter);
+                }
+//                System.out.println(builder.toString());
+                LOGGER.debug(builder.toString());
+            }
+
             // Obtain top K labelings, each scored with a probability
-            final Counter<List<CoreLabel>> labelings = this.ner.classifyKBest(tokens,
+            final Counter<List<CoreLabel>> labelings = this.ner4KBest.classifyKBest(tokens,
                     NamedEntityTagAnnotation.class, this.maxLabelings);
+
+//              // sort the labellings: https://stackoverflow.com/a/42916649
+//              List<List<CoreLabel>> sortedLabelings = Counters.toSortedList(labelings);
+
 
             // Map labelings to <span, scored tags> entities
             final Map<Span, Counter<String>> entities = extractEntities(labelings);
 
-            // Resolve overlappings by dropping low-priority spans
+            // filter entities: keep only those whose span is among the ones of the Standard NER annotation
             final int numEntitiesBefore = entities.size();
-            filterEntities(entities, this.minSpanConfidence);
+            filterEntitiesWithStandardNER(entities, entitiesStandard, 0.0, this.normalizeProbabilities);
+
+
+//            // OLD CODE: Resolve overlappings by dropping low-priority spans
+//            final int numEntitiesBefore = entities.size();
+//            filterEntities(entities, this.minSpanConfidence);
 
             // Annotate input tokens
             for (final Entry<Span, Counter<String>> entry : entities.entrySet()) {
@@ -115,13 +173,15 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
                 for (int i = span.begin; i < span.end; ++i) {
                     final CoreLabel token = tokens.get(i);
                     token.set(ScoredNamedEntityTagsAnnotation.class, counter);
-                    token.set(NamedEntityTagAnnotation.class, tag);
-                    token.set(NormalizedNamedEntityTagAnnotation.class, tag);
+//                    // The next ones are already saved by the NER Standard
+//                    token.set(NamedEntityTagAnnotation.class, tag);
+//                    token.set(NormalizedNamedEntityTagAnnotation.class, tag);
                 }
             }
 
             // Log outcome, if enabled
             if (LOGGER.isDebugEnabled()) {
+//            if (true) {
                 final StringBuilder builder = new StringBuilder();
                 builder.append("NER annotation for \"").append(annotation).append("\":");
                 builder.append("\n  ").append(labelings.size()).append("/")
@@ -142,6 +202,7 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
                     builder.append(" = ").append(counter);
                 }
                 LOGGER.debug(builder.toString());
+//                System.out.println(builder.toString());
             }
 
         } catch (final RuntimeInterruptedException ex) {
@@ -155,7 +216,7 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
     public void doOneFailedSentence(final Annotation annotation, final CoreMap sentence) {
         final List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
         for (final CoreLabel token : tokens) {
-            final String tag = this.ner.backgroundSymbol(); // should be "O"
+            final String tag = this.ner4KBest.backgroundSymbol(); // should be "O"
             final Counter<String> counter = new ClassicCounter<String>();
             counter.setCount(tag, 1.0);
             token.set(ScoredNamedEntityTagsAnnotation.class, counter);
@@ -203,6 +264,10 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
 
     }
 
+
+
+
+
     private static Map<Span, Counter<String>> extractEntities(
             final Counter<List<CoreLabel>> labelingCounters) {
 
@@ -210,27 +275,66 @@ public class NERConfidenceAnnotator extends SentenceAnnotator {
         for (final List<CoreLabel> labeling : labelingCounters.keySet()) {
             final double expProb = labelingCounters.getCount(labeling);
             final double prob = Math.exp(expProb);
-            int index = 0;
-            while (index < labeling.size()) {
-                final int beginIndex = index++;
-                final String tag = labeling.get(beginIndex).get(NamedEntityTagAnnotation.class);
-                if (tag != null && !"O".equalsIgnoreCase(tag)) {
-                    while (index < labeling.size() && tag
-                            .equals(labeling.get(index).get(NamedEntityTagAnnotation.class))) {
-                        ++index;
-                    }
-                    final Span span = new Span(beginIndex, index);
-                    Counter<String> entityCounter = entities.get(span);
-                    if (entityCounter == null) {
-                        entityCounter = new ClassicCounter<>();
-                        entities.put(span, entityCounter);
-                    }
-                    entityCounter.incrementCount(tag, prob);
-                }
-            }
+            extractEntitiesFromSingleLabelling(entities, labeling, prob);
         }
         return entities;
     }
+
+    private static void extractEntitiesFromSingleLabelling(Map<Span, Counter<String>> entities, List<CoreLabel> labeling, double prob) {
+        int index = 0;
+        while (index < labeling.size()) {
+            final int beginIndex = index++;
+            final String tag = labeling.get(beginIndex).get(NamedEntityTagAnnotation.class);
+            if (tag != null && !"O".equalsIgnoreCase(tag)) {
+                while (index < labeling.size() && tag
+                        .equals(labeling.get(index).get(NamedEntityTagAnnotation.class))) {
+                    ++index;
+                }
+                final Span span = new Span(beginIndex, index);
+                Counter<String> entityCounter = entities.get(span);
+                if (entityCounter == null) {
+                    entityCounter = new ClassicCounter<>();
+                    entities.put(span, entityCounter);
+                }
+                entityCounter.incrementCount(tag, prob);
+            }
+        }
+    }
+
+
+
+    private static void filterEntitiesWithStandardNER(final Map<Span, Counter<String>> entities,
+                                                      final Map<Span, Counter<String>> entitiesStandard,
+                                                      final double minSpanConfidence, boolean normalize) {
+
+        // Do nothing if there are no entities
+        if (entities.isEmpty()) {
+            return;
+        }
+
+        // Compute <span, span confidence> pairs, keeping onluy spans whose confidence is above the
+        // threshold and are among the ones of the Standard NER;
+        int maxIndex = 0;
+//        final Counter<Span> spans = new ClassicCounter<>();
+        for (final Entry<Span, Counter<String>> entry : ImmutableList
+                .copyOf(entities.entrySet())) {
+            final Span span = entry.getKey();
+            final Counter<String> tags = entry.getValue();
+            final double confidence = Counters.L1Norm(tags);
+            if (!entitiesStandard.containsKey(span)||(confidence <= minSpanConfidence)) {
+                entities.remove(span);
+            } else if (normalize) {
+                for (String tag:tags.keySet()
+                     ) {
+                    tags.setCount(tag,tags.getCount(tag)/confidence);
+                }
+                entities.put(span,tags);
+            }
+        }
+    }
+
+
+
 
     private static void filterEntities(final Map<Span, Counter<String>> entities,
             final double minSpanConfidence) {
